d10d4e1248c55ff94e0778ba6748a56749638212
Eduardo Rodrigues <edu.crod@gmail.com>
2021-05-11 01:55:32 -0300

    progress

--- mgmacbundle.py
+++ mgmacbundle.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 import subprocess
 import shlex
+import shutil
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("backuping old versions")
+
+def get_project_name():
+    with open("app.manifest", 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        if line.lstrip().startswith("<assemblyIdentity"):
+            project_name = line.partition("name=\"")[2].rstrip('\n\"/>').title()
+            print(project_name)
+            
+
+if os.path.isdir("bin/Release/osx-64/"):
+    print("lele")
+else:
+    print("lala")
 
 publish_command = shlex.split("dotnet publish -c Release -r osx-x64 /p:PublishReadyToRun=false /p:TieredCompilation=false --self-contained")
 
 try:
     subprocess.run(publish_command,check=True, capture_output=True)
 except subprocess.CalledProcessError as err:
-    print(err.output)
+    logging.error(str(err.output))
 
+
+get_project_name()
